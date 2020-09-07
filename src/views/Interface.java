681700c3e3ce314d87d5e09a091c1db18743c720
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-09-07 20:46:58 +0200

    view implemented

--- src/views/Interface.java
+++ src/views/Interface.java
@@ -2,6 +2,8 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -10,24 +12,40 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 
 public class Interface extends JFrame{
 	
+	JPanel panel;
+	
 	public Interface() {
 		super();
 		this.setTitle("Request Tester");
 		JButton button;
-		JTextField queryText = new JTextField();
-		queryText.setSize(300, 100);
 		JToolBar toolbar = new JToolBar();
 		button = new JButton("Run query",new ImageIcon("icons/play.png"));
 		toolbar.add(button);
         this.add(toolbar, BorderLayout.NORTH);
-        this.add(queryText, BorderLayout.CENTER);
+        this.add(createMainPanel(), BorderLayout.CENTER);
 		this.setSize(1200, 700);
 		this.setVisible(true);
+	}
+	
+	public JPanel createMainPanel() {
+		panel = new JPanel();
+		JTextArea queryText = new JTextArea();
+		queryText.setColumns(95);
+		queryText.setRows(10);
+		queryText.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,15));
+		JTable table = new JTable();
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT,queryText,table);
+		panel.add(splitPane);
+		return panel;
 	}
 
 }
